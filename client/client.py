5cc52600a02d9ba3aae42f8121e72dcb4f6b9e29
GIider <marcel.carl.hagenbach@googlemail.com>
2013-12-19 22:53:20 +0100

    Add the UDP server and UDP client

---
+++ client/client.py
@@ -0,0 +1,13 @@
+import socket
+
+
+class RemoteLedStripe(object):
+    def __init__(self, host, port=26667):
+        self.host = host
+        self.port = port
+
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def send_hex_string(self, hex_string):
+        """Send a hex string to the server to change the colors on the stripe"""
+        self.socket.sendto(bytes(hex_string + "\n", "utf-8"), (self.host, self.port))
---
+++ server/server.py
@@ -0,0 +1,37 @@
+"""Small socket server to interact with the led stripe from a remote client
+
+This should be called with the amount of leds in the stripe as argument
+"""
+import sys
+
+try:
+    import SocketServer as socketserver
+except ImportError:
+    import socketserver
+
+from controller import LedStripe
+
+
+class HexLedStripeUDPHandler(socketserver.BaseRequestHandler):
+    """A server that accepts hex strings to change the local LED stripe"""
+    __slots__ = 'stripe'
+
+    def handle(self):
+        data = self.request[0].strip()
+
+        stripe = self.server.stripe
+
+        stripe.set_all_pixels(data)
+        stripe.update()
+
+
+if __name__ == "__main__":
+    HOST, PORT = "", 26667
+
+    num_leds = int(sys.argv[1])
+
+    server = socketserver.UDPServer((HOST, PORT), HexLedStripeUDPHandler)
+    server.stripe = LedStripe(num_leds)
+
+    print('Now serving on port %d' % PORT)
+    server.serve_forever()
